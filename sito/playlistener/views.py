70597676f2dc92d36d83ff773ebad0130f8f941f
Emaiarkeiou <mirkomaralit@gmail.com>
2024-04-23 00:31:46 +0200

    non so

--- sito/playlistener/views.py
+++ sito/playlistener/views.py
@@ -7,4 +7,4 @@ from .models import Album, Artista, Canzone, Playlist
 def index(request):
     """View function for home page of site."""
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'base.html', context={"a":2})
+    return render(request, 'base_log.html', context={"a":2})
