21380a174a13522912a72a584335ca134019ac4b
Emaiarkeiou <mirkomaralit@gmail.com>
2024-04-27 19:25:04 +0200

    modal pfp

--- sito/playlistener/views.py
+++ sito/playlistener/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from django.views.decorators.cache import cache_control
+
 
 # Create your views here.
 
 from .models import Album, Artista, Canzone, Playlist, Utente
+
 
 """
 
@@ -16,27 +19,38 @@ POST PER LE FORM E BOTTONI SUBMIT(per input)
 """
 
 
-
+@cache_control(must_revalidate=True, no_store=True)
 def loginView(request):
     """View function for home page of site."""
     if request.method == 'GET':
         """ GET della pagina di login """
-        return render(request, 'registration/login.html')
+        if request.user.is_authenticated:
+            return redirect(userView,request.user.username)
+        else:
+            return render(request, 'registration/login.html')
     elif request.method == 'POST':
         """ POST della form di login """
         username = request.POST['username'].lower()
         password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect(userView, username=username) 
+        if username and password:
+            try:
+                user = User.objects.get(username=username)
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect(userView, username=username) 
+                else:
+                    error = "Password errata"
+            except User.DoesNotExist:
+                error = "Credenziali errate"
         else:
-            return redirect(loginView)
+            error = "Riempi tutti i campi"
+        return render(request, 'registration/login.html',context={"error":error,"username":username,"password":password})
     else:
         pass
     """ REDIRECT AD UNA PAGINA CHE DICE CHE HAI SBAGLIATO"""
 
-
+@cache_control(must_revalidate=True, no_store=True)
 def signupView(request):
     """View function for home page of site."""
     if request.method == 'GET':
@@ -49,24 +63,14 @@ def signupView(request):
         username = request.POST['username'].lower()
         password = request.POST['password']
         if nome and cognome and username and password:
-            if len(password)>=8:
+            if len(username<3):
+                error = "Username troppo corto (min 3)"
+            elif len(password)>=8:
                 try:
                     User.objects.create_user(username=username, password=password, first_name=nome, last_name=cognome)
                     user = authenticate(username=username, password=password)
                     Utente.objects.create(user=user)
                     login(request, user)                                                            
-
-
-
-
-                    """ GUARDARE COME TOGLIERE BACK BUTTON"""#AAAAAAAAAAAAAAAAAAAAA
-
-
-
-
-
-
-
                     return redirect(userView, username=username)
                 except IntegrityError:
                     error = username + " esiste già"
@@ -89,6 +93,7 @@ def logoutView(request):
         """ REDIRECT AD UNA PAGINA CHE DICE CHE HAI SBAGLIATO"""
 
 
+@cache_control(must_revalidate=True, no_store=True)
 def userView(request,username):
     """View function for home page of site."""
     if request.method == 'GET': #get_object_or_404(MyModel, pk=1)
@@ -97,11 +102,7 @@ def userView(request,username):
                 utente = User.objects.get(username=username) 
                 lista_playlist = Playlist.objects.filter(user_id=utente.id)
                 return render(request, 'user.html', context={"username":username,"playlists":lista_playlist})
-            else:
-                """ REDIRECT AD UNA PAGINA CHE DICE CHE HAI SBAGLIATO"""
-                return redirect(loginView)
-        else:
-            return redirect(loginView)
+        return redirect(loginView)
     else:
         pass
         """ REDIRECT AD UNA PAGINA CHE DICE CHE HAI SBAGLIATO"""
